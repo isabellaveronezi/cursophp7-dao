0a9a7ab75ea4227edf7fb64fa2dbc39083c2476b
Isabella Veronezi <isabella.veronezi@webjump.com.br>
2019-07-30 11:29:45 -0300

    método de lista

--- index.php
+++ index.php
@@ -1,20 +1,25 @@
 <?php 
 
     require_once("config.php");
+
 //Carrega um usuário
 //$root = new Usuario();
 //$root->loadbyId(3);
-//echo $root;
+//echo $root; 
+
 //Carrega uma lista de usuários
-//$lista = Usuario::getList();
+//$lista = Usuario::getList(); // chama direto porque é um método static
 //echo json_encode($lista);
+
 //Carrega uma lista de usuários buscando pelo login
 //$search = Usuario::search("jo");
 //echo json_encode($search);
+
 //carrega um usuário usando o login e a senha
 //$usuario = new Usuario();
-//$usuario->login("root", "!@#$");
+//$usuario->login("Isabella", "1234");
 //echo $usuario;
+
 /*
 //Criando um novo usuário
 $aluno = new Usuario("aluno", "@lun0");
@@ -29,9 +34,8 @@ $usuario->update("professor", "!@#$%¨&*");
 echo $usuario;
 */
     die("passou por aqui");
-    $root = new Usuario(); 
-    $root->loadById(2);
-    echo $root; 
-
+    $usuario = new Usuario();
+    $usuario->login("Isabella", "1234");
+    echo $usuario;
 
 ?>
